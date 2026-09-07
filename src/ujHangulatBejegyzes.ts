caee5c7f39097aecef4af16c4dddd25a28e625a1
Török Donát <torok.donat07@gmail.com>
2026-09-07 09:00:05 +0200

    ujHangulatBejegyzes backend

--- src/ujHangulatBejegyzes.ts
+++ src/ujHangulatBejegyzes.ts
@@ -1,1 +1,30 @@
 import './style.css'
+
+async function formKezeles(e: Event) {
+    e.preventDefault();
+    const form: HTMLFormElement = document.getElementById("hangulatForm") as HTMLFormElement;
+    const formData = new FormData(form);
+
+    const hangulat = formData.get("hangulat");
+    const leiras = formData.get("leiras");
+
+
+    const response = await fetch(import.meta.env.VITE_URL_LINK,
+        {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify({ hangulat:hangulat, leiras:leiras})
+        })
+    
+    if (!response.ok) {throw new Error("Feltöltés nem sikerült "+response.statusText)}
+
+    form.reset();
+}
+
+function init() {
+    document.getElementById("hangulatForm")?.addEventListener("submit", formKezeles);
+}
+
+document.addEventListener("DOMContentLoaded", init);
